88c944a284b05ebe13f9bb68a17fa7126278b1cc
Shubham-Chaudhary73 <shubhamchaudharyrmp102001@gmail.com>
2026-07-04 14:10:25 +0530

    Modified or Added new file

--- 02_DataTypes.js
+++ 02_DataTypes.js
@@ -1,1 +1,21 @@
-console.log("Datatye")
+"use strict"// treat all js code as newer version 
+// alert(3+3) // we are using node js not broswer
+
+let name="Shubham"
+let age=23
+let isLoggedIn=false
+
+// number 
+// bigint
+//string  =""
+// boolean = true /false
+// null // standalone value
+// undefined
+// symbol  - unique
+
+
+// Non Primitive data tpye 
+// object
+
+console.log(typeof null)// object 
+console.log(typeof undefined)// undefined
