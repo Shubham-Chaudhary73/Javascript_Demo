c150b9d9bc3de1b6fe4954476745b460b734173a
Shubham-Chaudhary73 <shubhamchaudharyrmp102001@gmail.com>
2026-07-05 16:16:27 +0530

    Data type conversion

--- 02_DataTypes.js
+++ 02_DataTypes.js
@@ -1,9 +1,9 @@
 "use strict"// treat all js code as newer version 
 // alert(3+3) // we are using node js not broswer
 
-let name="Shubham"
-let age=23
-let isLoggedIn=false
+//let name="Shubham"
+//let age=23
+//let isLoggedIn=false
 
 // number 
 // bigint
@@ -17,5 +17,28 @@ let isLoggedIn=false
 // Non Primitive data tpye 
 // object
 
-console.log(typeof null)// object 
-console.log(typeof undefined)// undefined
+//console.log(typeof null)// object 
+//console.log(typeof undefined)// undefined
+/*let score=undefined
+let valueInNumber=Number(score)
+console.log(typeof score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+*/
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+/*
+let isLoggedIn="Shubham"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+*/
+
+// 1=> true ; 0 => false
+// "" => false ;"Shubham" => true
+let someNumber=33
+console.log(someNumber)
+console.log(typeof someNumber)
