93bd860e7721a1631988ce9e29875299d7adff36
Shubham-Chaudhary73 <shubhamchaudharyrmp102001@gmail.com>
2026-07-05 17:49:48 +0530

    Strings explaination

--- 03_Strings.js
+++ 03_Strings.js
@@ -1,1 +1,10 @@
-console.log("String")
+/*const name ="Shubham"
+const repoCount=1
+//console.log(name+repoCount+" value")
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+*/
+
+const gameName=new String("Shubham Chaudhary")
+//console.log(gameName[0])
+//console.log(gameName.__proto__)
+console.log(gameName.toUpperCase())
